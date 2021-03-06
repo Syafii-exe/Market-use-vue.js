d2516b3f55c636fe51d6e27fc14b73561be77be1
Muhammad Syafii Ramadhan <37789994+Syafii-exe@users.noreply.github.com>
2021-03-06 18:46:03 +0700

    Update HeaderMarket.vue

--- src/components/HeaderMarket.vue
+++ src/components/HeaderMarket.vue
@@ -5,7 +5,7 @@
                 <div class="container">
                     <div class="ht-left">
                         <div class="mail-service">
-                            <i class=" fa fa-envelope"></i> hello.shayna@gmail.com
+                            <i class=" fa fa-envelope"></i> hello.Mr.
                         </div>
                         <div class="phone-service">
                             <i class=" fa fa-phone"></i> +628 22081996
